c334ab8dba434432c124226d49ca1ea0a3bfeb1c
Na-veed <naveedimran2877@gmail.com>
2021-09-09 15:42:53 +0530

    My first git commit on employee app

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Employee-docker-jenkins" 
---
+++ src/main/java/Employee_docker_jenkins/Employee_docker_jenkins/App.java
@@ -0,0 +1,13 @@
+package Employee_docker_jenkins.Employee_docker_jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
